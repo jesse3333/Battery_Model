b129990f18e0ed2f5c4c1e3f2c806c9f76ed5cb7
jesse3333 <jschulthess3@gmail.com>
2022-05-24 20:45:00 -0600

    show info on dataframe df1

--- EV_Data_Analysis_and_viz.py
+++ EV_Data_Analysis_and_viz.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
-file = r'C:\\Users\\jschu\\Desktop\\Coding Stuff\\Battery_Model\\EV_Data\\TripA01.csv'
+file = r'C:\\Users\\jschu\\Desktop\\Coding Stuff\\Battery_Model_data\\EV_Data\\TripA01.csv'
 
 df1 = pd.read_csv(file,sep=';',encoding='ISO-8859-1')
 print(df1.head())
+print(df1.info())
