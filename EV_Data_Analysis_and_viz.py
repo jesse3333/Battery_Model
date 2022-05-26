74cb27680eacb444b500be92dc868930addf1b4e
jesse3333 <jschulthess3@gmail.com>
2022-05-25 21:10:29 -0600

    added describe so I could see general data stats

--- EV_Data_Analysis_and_viz.py
+++ EV_Data_Analysis_and_viz.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-file = r'C:\\Users\\jschu\\Desktop\\Coding Stuff\\Battery_Model_data\\EV_Data\\TripA01.csv'
+file = r'C:\\Users\\jschu\\Desktop\\Jesse\\Coding Stuff\\Battery_Model_data\\EV_Data\\TripA01.csv'
 
 df1 = pd.read_csv(file,sep=';',encoding='ISO-8859-1')
 print(df1.head())
 print(df1.info())
+print(df1.describe())
